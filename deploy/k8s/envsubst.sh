44d5bf017eb9c04fff27f476d1dfb3060e99c651
Tobias Dußmann <tobias.dussmann@gmail.com>
2018-02-06 03:06:15 +0100

    cleanup k8s deployment yaml and changed envsubst.sh to accept buildnumber (image tag) as single argument

--- deploy/k8s/envsubst.sh
+++ deploy/k8s/envsubst.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed -e "s/\${BUILD_NUMBER}/${CI_BUILD_NUMBER}/" deploy-CI.template.yaml  > deploy-CI.yaml
+# call with build number (image tag for ci built docker image) as single argument
+sed -e "s/\${BUILD_NUMBER}/$1/" deploy-CI.template.yaml  > deploy-CI.yaml
 nl deploy-CI.yaml
